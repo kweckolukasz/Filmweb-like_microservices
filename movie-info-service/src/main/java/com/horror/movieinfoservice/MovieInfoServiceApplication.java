e688d8449c78633bf64f4c4251b8f4f3eb339b29
kweckolukasz <kweckolukasz@gmail.com>
2021-02-11 10:19:17 +0100

    Eureka client disabled

--- movie-info-service/src/main/java/com/horror/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/horror/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,17 +3,17 @@ package com.horror.movieinfoservice;
 import com.horror.movieinfoservice.data.MovieService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class MovieInfoServiceApplication {
 
 	@Bean
-	@LoadBalanced
+//	@LoadBalanced
 	RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
